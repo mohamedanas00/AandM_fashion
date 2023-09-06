cc1762bd0a5b57a2e3e1ca77f012e5dc147c916c
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-09-07 01:12:28 +0300

    Atlas DB

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    return await mongoose.connect(process.env.DB)
+    return await mongoose.connect(process.env.DB_ATLAS)
         .then(res => console.log(`DB Connected successfully✅ .........`))
         .catch(err => console.log(` Fail to connect  DB❌.........${err} `))
 
