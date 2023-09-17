acfc570da1d25f4cc3b1c01a11e99cdc41702efb
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-09-17 14:15:16 +0300

    add cors

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import path from 'path'
 import express from 'express'
 import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
+var cors = require('cors')
 const app = express()
 //set directory dirname 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 dotenv.config({ path: path.join(__dirname, './config/.env') })
 import initApp from './src/index.routes.js'
+//cors 
+app.use(cors())
 // setup port and the baseUrl
 initApp(app, express)
+
 const port = +process.env.PORT
 app.listen(port, () => console.log(`App listening on port:${port}!`))
