8a0a92942bd8e18ef920e86aae10c55ff724445f
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-09-18 23:54:28 +0300

    expire token

--- src/utils/generateAndVerifyToken.js
+++ src/utils/generateAndVerifyToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (payload = {}) => {
     const signature = process.env.TOKEN_SIGNATURE
-    const expiresIn = 60 * 60
+    const expiresIn = Math.floor(Date.now() / 1000) + 30 * 24 * 60 * 60;
     const token = jwt.sign(payload, signature, { expiresIn: parseInt(expiresIn) });
     return token
 }
