63fd455eaa159a8f106fc087ae82fa885845b78b
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-09-22 23:17:17 +0300

    clean code for auth

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -56,29 +56,3 @@ export const restPassword = {
 
 }
 
-export const updateProfile={
-    params: joi.object().required().keys({}),
-    body: joi.object({
-        name: generalFields.name.min(3).max(30),
-        email: joi.string().email({
-            minDomainSegments: 2,
-            maxDomainSegments: 4,
-            tlds: { allow: ['com', 'net'] }
-        }),
-        phone: joi.string().trim().pattern(/^(010|012|011|015)\d{8}$/),
-        birthday: joi.date(),
-    }),
-    query: joi.object().required().keys({}),
-}
-
-export const deleteAccount={
-    params: joi.object().required().keys({}),
-    body: joi.object().required().keys({}),
-    query: joi.object().required().keys({}),
-}
-
-export const getUserData={
-    params: joi.object().required().keys({}),
-    body: joi.object().required().keys({}),
-    query: joi.object().required().keys({}),
-}
