2f904bb2c957ab1dd812f00df97862873b0a5fba
mohamedanas00 <mohamed.anas7688@gmail.com>
2023-09-22 23:14:53 +0300

    clean code for auth

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -108,47 +108,6 @@ export const restPassword = asyncHandler(async (req, res, next) => {
     return res.status(StatusCodes.ACCEPTED).json({ message: "Done" })
 })
 
-export const delteAccount = asyncHandler(async (req, res, next) => {    
-    const userId = req.user._id
-    const user = new userModel({_id: userId})
-    await user.deleteOne({ _id: userId })
-    return res.status(StatusCodes.ACCEPTED).json({ message: "Done" })
-})
-
-export const updateProfile = asyncHandler (async (req,res,next)=>{
-    let {name , email ,phone ,birthday} = req.body
-    const id = req.user._id
-    if(email){
-        const isEmailExist = await userModel.findOne({ email: req.body.email })
-        if (isEmailExist) {
-            return next(new ErrorClass(`This email:"${req.body.email}" Already Exist!`, StatusCodes.CONFLICT))
-        }
-    }
-    if(phone){
-        phone = CryptoJS.AES.encrypt(phone, process.env.encrypt_key).toString()
-    }
-    await userModel.updateOne({_id:id},{
-        name , 
-        email,
-        phone,
-        birthday
-    })
-    return res.status(StatusCodes.ACCEPTED).json({message:"Done"})
-
-})
-
-export const getUserData = asyncHandler(async(req,res,next)=>{
-    if(req.user.phone){
-        req.user.phone= CryptoJS.AES.decrypt(req.user.phone, process.env.encrypt_key).toString(CryptoJS.enc.Utf8);
-    } 
-    return res.status(StatusCodes.OK).json({
-        name:req.user.name,
-        email:req.user.email,
-        phone:req.user.phone,
-        birthday:req.user.birthday
-    })
-})
-
 // export const socialLogin =asyncHandler( async (req,res,next)=>{
 //     const {idToken,phone,birthday} =req.body
 //     const ticket = await client.verifyIdToken({
